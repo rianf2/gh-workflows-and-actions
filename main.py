f1f0c9c8b021e3d9c76b26a584e90758df947a0c
Rian <rianf2@users.noreply.github.com>
2026-04-21 14:13:09 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import unittest
+
+class Number:
+    def __init__(self, value):
+        this.value = value
+
+    def get_value(self):
+        return this.value
+
+class TestNumber(unittest.TestCase):
+    def test_get_value(self):
+        number = Number(420)
+        self.assertEqual(420, number.get_value())
+
+if __name__ == '__main__':
+    unittest.main()
+

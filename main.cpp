b53680f165343f81cfa025e3cca9df5b11548078
Rian <rianf2@users.noreply.github.com>
2026-04-21 11:46:02 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+class Number
+{
+private:
+    int m_Value{0};
+public:
+    Number(const int value) : m_Value(value) {}
+    int getValue() const { return m_Value; }
+    void printValue() { std::cout << "Value is: " << this->getValue(); }
+};
+
 int main(int argc, char **argv)
 {
     std::cout << "Hello World" << "\n";
+
+    Number n{420};
+    
+    n.printValue();
 }
